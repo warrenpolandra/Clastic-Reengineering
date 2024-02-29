b7d5fe8c9501f306b983becf5e84f95778e9eeff
Warren Gerald Polandra <85095564+warrenpolandra@users.noreply.github.com>
2024-02-29 16:38:51 +0800

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Clastic Reengineering"
+include(":app")
+ 
