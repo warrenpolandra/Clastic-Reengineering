350f235c5ccb8f8ef89199cdb2513a9711002654
Warren Gerald Polandra <85095564+warrenpolandra@users.noreply.github.com>
2024-04-15 17:47:48 +0800

    [development-tutorial] feat: add tutorial screen

---
+++ feature/home/src/main/java/com/clastic/home/component/BenefitCard.kt
@@ -0,0 +1,69 @@
+package com.clastic.home.component
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.clastic.ui.theme.ClasticTheme
+import com.clastic.ui.theme.CyanPrimary
+import com.clastic.ui.theme.CyanTextField
+
+@Composable
+fun BenefitCard(
+    benefitText: String,
+    cardNumber: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+            .width(105.dp)
+            .height(140.dp)
+            .border(
+                width = 1.dp,
+                CyanTextField,
+                shape = RoundedCornerShape(20.dp)
+            )
+            .padding(8.dp)
+    ) {
+        Text(
+            text = benefitText,
+            color = Color.Black,
+            fontSize = 13.sp
+        )
+        Text(
+            text = cardNumber.toString(),
+            textAlign = TextAlign.End,
+            style = MaterialTheme.typography.h5.copy(
+                color = CyanPrimary,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BenefitCardPreview() {
+    ClasticTheme {
+        BenefitCard(
+            benefitText = "Meningkatkan Kesehatan Lingkungan",
+            cardNumber = 1
+        )
+    }
+}
---
+++ feature/home/src/main/java/com/clastic/home/component/ExchangeStep.kt
@@ -0,0 +1,70 @@
+package com.clastic.home.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.clastic.ui.theme.ClasticTheme
+import com.clastic.ui.theme.CyanPrimaryVariant2
+
+@Composable
+fun ExchangeStep(
+    stepNumber: Int,
+    stepText: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.Top,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .width(24.dp)
+                .height(24.dp)
+                .clip(CircleShape)
+                .background(CyanPrimaryVariant2),
+        ) {
+            Text(
+                text = stepNumber.toString(),
+                style = MaterialTheme.typography.subtitle1.copy(
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+        Text(
+            text = stepText,
+            color = Color.Black
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ExchangeStepPreview() {
+    ClasticTheme {
+        ExchangeStep(
+            stepNumber = 1,
+            stepText = "Click this"
+        )
+    }
+}
